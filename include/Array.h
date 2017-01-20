6a9c639fd436f77c320fe454776190da31c4f30c
betonildo <gilbertoribeiropazdarosa@gmail.com>
2017-01-19 23:52:43 -0200

    [chg] array uses it's own instance of Memallocator as allocator

--- include/Array.h
+++ include/Array.h
@@ -1,24 +1,28 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include <Memallocator.h>
-#include <MemTypes.h>
+#include <CustomTypes.h>
 
-template<class T, Memallocator* mem>
+template<class T, class Allocator>
 class Array {
 
     T** array;
     U64 count;
-    
+    Allocator allocator;
+
 public:
 
     Array(size_t count) {
-        array = mem->allocateArray<T>(count);
+        array = (T**)allocator.allocate(sizeof(T) * count);
         this->count = count;
     }
 
     T& operator[](unsigned int index) {
         return array[0][index];
+    }
+
+    T* operator&() {
+        return array[0];
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <Definitions.h>
+#include <Array.h>
+
+struct Person {
+    byte age;
+    byte height;
+};
 
 int main(int argc, char** argv) {
-
-    std::cout << "LTL Working!\n";
-
+    
     return 0;
 }
